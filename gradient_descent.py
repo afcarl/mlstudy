6e7f7a29429f01c102883f929296d7827fe6713f
jinyeom <jinyeom95@gmail.com>
2017-01-12 17:55:57 -0600

    progress

---
+++ gradient_descent.py
@@ -0,0 +1,16 @@
+# module:   gradient_descent.py
+# author:   Jin Yeom
+# since:    01/12/17
+
+import numpy as np
+
+n = 4   # number of features
+m = 20  # number of training examples
+
+# hypothesis function
+h = lambda theta, x: np.dot(theta.T, x)
+
+# cost function
+j = lambda x, y, theta: 
+
+dataset = np.random.rand()
